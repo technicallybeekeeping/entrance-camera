338eb8b0ecad161d933aec1c1a43a56e246f334f
Michael Erpenbeck <support@technicallybeekeeping.com>
2024-04-03 17:39:33 -0500

    Update install script

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 pip install schedule
-crontab -l | { cat; echo "@reboot sudo /usr/bin/python /home/techbee/Desktop/entrance-camera/src/TechBeeCam.py"; } | crontab -
+
+crontab -l | { cat; echo "@reboot cd /home/techbee/Desktop/entrance-camera/src/; sudo -E /usr/bin/python TechBeeCam.py"; } | crontab -
+
+/usr/bin/thonny /home/techbee/Desktop/entrance-camera/src/config.py
